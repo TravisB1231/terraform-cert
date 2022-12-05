df4af6ea75ae04fe34b625ae8053caa9a5d91c2c
Kyrreth <travisrbrown1231@gmail.com>
2022-12-04 19:47:10 -0500

    depends_on

--- babys-first-tf/babys-first-tf.tf
+++ babys-first-tf/babys-first-tf.tf
@@ -8,3 +8,16 @@ resource "random_pet" "my-pet-2" {
   separator = var.separator
   length = var.length
 }
+
+resource "local_file" "whale" {
+  filename = "./whale.txt"
+  content = "Whale's need krill to survive."
+
+  depends_on = [
+    local_file.krill
+  ]
+}
+resource "local_file" "krill" {
+  filename = "./krill.txt"
+  content = "Tasty whale food."
+}
